1cd75069eb7e11d6014dd9ac142076252f707ec8
Scott McPeak <scott.g.mcpeak@gmail.com>
2024-06-06 03:22:08 -0700

    Add clang-file-entry-fwd.h

---
+++ clang-file-entry-fwd.h
@@ -0,0 +1,12 @@
+// clang-file-entry-fwd.h
+// Forwards for `clang/Basic/FileEntry.h`.
+
+#ifndef PCA_CLANG_FILE_ENTRY_FWD_H
+#define PCA_CLANG_FILE_ENTRY_FWD_H
+
+namespace clang {
+  class FileEntryRef;
+  class FileEntry;
+}
+
+#endif // PCA_CLANG_FILE_ENTRY_FWD_H
