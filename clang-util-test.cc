abf04d812ef3bd4bc44718d4cc0979993d534733
Scott McPeak <scott.g.mcpeak@gmail.com>
2024-06-14 00:19:35 -0700

    clang-util-test.cc: Add a few more tests of `getInstFromDeclOpt`.

--- clang-util-test.cc
+++ clang-util-test.cc
@@ -75,6 +75,9 @@ void GIFDVisitor::visitDecl(
 
 void testOneGetInstFromDeclOpt(char const *source, GDValue const &expect)
 {
+  // Clang has a virtual file system that could be used to avoid
+  // actually hitting the disk here, but after a quick look, I decided
+  // it appeared to be more trouble than I wanted to deal with.
   TemporaryFile temp("gifd", "cc", source);
   ClangASTUtil ast({temp.getFname()});
 
@@ -151,6 +154,73 @@ void testGetInstFromDeclOpt()
     })
   );
 
+  // Class template contains function template.
+  testOneGetInstFromDeclOpt(
+    R"(
+      template <typename T>
+      struct S {
+        template <typename U>
+        void m1() {}
+      };
+
+      void g()
+      {
+        S<int> s1;
+        s1.m1<float>();
+      }
+    )",
+    GDValue(GDVSet{
+      GDVTuple{"S<int>",              "S<T>"},
+      GDVTuple{"S<int>::m1<float>()", "S::m1<U>()"},
+    })
+  );
+
+  // Class template contains ordinary class.
+  testOneGetInstFromDeclOpt(
+    R"(
+      template <typename T>
+      struct S {
+        struct Inner {
+          void m() {}
+        };
+      };
+
+      void g()
+      {
+        S<int>::Inner i;
+        i.m();
+      }
+    )",
+    GDValue(GDVSet{
+      GDVTuple{"S<int>",              "S<T>"},
+      GDVTuple{"S<int>::Inner",       "S::Inner"},
+      GDVTuple{"S<int>::Inner::m()",  "S::Inner::m()"},
+    })
+  );
+
+
+  // Class template contains class template.
+  testOneGetInstFromDeclOpt(
+    R"(
+      template <typename T>
+      struct S {
+        template <typename U>
+        struct Inner {
+          void m() {}
+        };
+      };
+
+      void g()
+      {
+        S<int>::Inner<float> i;
+      }
+    )",
+    GDValue(GDVSet{
+      GDVTuple{"S<int>",                    "S<T>"},
+      GDVTuple{"S<int>::Inner<float>",      "S::Inner<U>"},
+      GDVTuple{"S<int>::Inner<float>::m()", "S::Inner::m()"},
+    })
+  );
 
 
   // TODO: More tests.
