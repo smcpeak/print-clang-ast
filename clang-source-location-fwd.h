617322a0347c25f22e6a370dbf4530a713cabc7f
Scott McPeak <scott.g.mcpeak@gmail.com>
2024-06-22 02:02:33 -0700

    Add clang-source-location-fwd.h.

---
+++ clang-source-location-fwd.h
@@ -0,0 +1,16 @@
+// clang-source-location-fwd.h
+// Forwards for clang/Basic/SourceLocation.h.
+
+#ifndef PCA_CLANG_SOURCE_LOCATION_FWD_H
+#define PCA_CLANG_SOURCE_LOCATION_FWD_H
+
+namespace clang {
+  class FileID;
+  class SourceLocation;
+  class SourceRange;
+  class CharSourceRange;
+  class PresumedLoc;
+  class FullSourceLoc;
+}
+
+#endif // PCA_CLANG_SOURCE_LOCATION_FWD_H
